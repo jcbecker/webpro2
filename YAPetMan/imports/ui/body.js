6676d861b398e95fc4de876bcbb1819fc8bb0382
jcbecker <joaocarlosbecker666@gmail.com>
2017-07-06 18:01:59 -0300

    melhorias na vizualização de dados

--- YAPetMan/imports/ui/body.js
+++ YAPetMan/imports/ui/body.js
@@ -5,7 +5,7 @@ import { Rpets } from '../api/items.js';
 import './body.html';
 
 
-Template.footer.helpers({
+Template.registredspet.helpers({
     pet(){
         return  Rpets.find({}); 
         
@@ -21,12 +21,16 @@ Template.body.events({
     // Get value from form element
     const target = event.target;
     const name = target.petName.value;
+    const gender = target.gender.value;
+    const species = target.species.value;
     
     // Insert a task into the collection
     Rpets.insert({
         owner: Meteor.userId(),
         username: Meteor.user().username,
         name,
+        gender,
+        species,
         createdAt: new Date(), // current time
     });
     
