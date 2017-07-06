cc83b468b4418c6142dc91972aa5add2d2be1576
jcbecker <joaocarlosbecker666@gmail.com>
2017-07-06 12:16:49 -0300

    lendo dados de formulário, e banco de dados

---
+++ YAPetMan/imports/api/items.js
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/mongo';
+
+export const Rpets = new Mongo.Collection('rpets');
---
+++ YAPetMan/imports/ui/body.js
@@ -0,0 +1,34 @@
+import { Template } from 'meteor/templating';
+import { Rpets } from '../api/items.js';
+
+import './body.html';
+
+
+Template.footer.helpers({
+    pet(){
+        return  Rpets.find({}); 
+        
+    }
+});
+
+Template.body.events({
+  'submit .petRegister'(event){
+    // Prevent default browser form submit
+    event.preventDefault();
+    
+    
+    // Get value from form element
+    const target = event.target;
+    const name = target.petName.value;
+    
+    // Insert a task into the collection
+    Rpets.insert({
+      name,
+      createdAt: new Date(), // current time
+    });
+    
+    
+    // Clear form
+    target.petName.value = '';
+  },
+});
